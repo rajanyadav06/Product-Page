3772491fb48071ef594d6cc7543526d70ffba408
Rajan Yadav <rajanwebstudio@gmail.com>
2025-02-17 17:36:22 +0530

    Add

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Product from './Components/Product'
+function App() {
+
+  return (
+    <>
+      <h1 className=' flex items-center justify-center'>Hello Team. <span className=' font-bold text-3xl'>4</span></h1>
+      <Product />
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Product.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Product = () => {
+  return (
+    <>
+      <h1 className=" flex items-center justify-center h-20">Product Component</h1> 
+    </>
+  )
+};
+
+export default Product;
